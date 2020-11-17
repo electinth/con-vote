aab862954b69d64d160de644f922c7e1ab451b04
Rapee Suveeranont <iiieyes@gmail.com>
2020-11-17 14:29:57 +0700

    Set route path to /con-vote

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/con-vote'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'con-vote',
